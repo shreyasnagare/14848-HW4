945106b43bafce654f43361eeecf266dfbddb362
shreyasnagare <shreyasnagare@gmail.com>
2021-11-18 15:30:35 -0500

    Add reducer.py

---
+++ reducer.py
@@ -0,0 +1,25 @@
+import sys
+import math
+
+current_yearMonthDay = None
+max_temperature = -math.inf
+
+for line in sys.stdin:
+    line = line.strip()
+    yearMonthDay, temperature = line.split(sep="\t", maxsplit=1)
+
+    try:
+        temperature = int(temperature)
+    except ValueError:
+        continue
+
+    if yearMonthDay == current_yearMonthDay:
+        max_temperature = max(temperature, max_temperature)
+    else:
+        if current_yearMonthDay:
+            print(f"{current_yearMonthDay}\t{max_temperature}")
+        current_yearMonthDay = yearMonthDay
+        max_temperature = temperature
+
+if current_yearMonthDay:
+    print(f"{current_yearMonthDay}\t{max_temperature}")
